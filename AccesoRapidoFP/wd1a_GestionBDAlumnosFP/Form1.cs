a55808353d194809ddfc38de7d85d76e9d5432df
ALUM01 <ALUM01@PC01>
2017-11-27 11:34:08 +0100

    VS

--- AccesoRapidoFP/wd1a_GestionBDAlumnosFP/Form1.cs
+++ AccesoRapidoFP/wd1a_GestionBDAlumnosFP/Form1.cs
@@ -7,14 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using wd1a_GestionBDAlumnosFP.DataSet1TableAdapters;
 
 namespace wd1a_GestionBDAlumnosFP
 {
     public partial class Form1 : Form
     {
+
+        //Crea el adaptador de tabla
+        GruposTableAdapter gruposAdapt = new GruposTableAdapter();
+
+        //Crea la tabla 
+        DataSet1.GruposDataTable gruposTabla = new DataSet1.GruposDataTable();
+
+        
         public Form1()
         {
             InitializeComponent();
+
+            // Cargamos la tabla Grupos
+            gruposAdapt.Fill(gruposTabla);
+            gruposTabla = gruposAdapt.GetData();
+
+            
+
         }
     }
 }
