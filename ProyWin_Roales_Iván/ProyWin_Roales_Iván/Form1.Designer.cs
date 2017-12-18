21e4ad77dda0e9330b035bb54a8333a7b007fce1
ALUM01 <ALUM01@PC01>
2017-12-18 13:58:37 +0100

    VS

--- ProyWin_Roales_Iván/ProyWin_Roales_Iván/Form1.Designer.cs
+++ ProyWin_Roales_Iván/ProyWin_Roales_Iván/Form1.Designer.cs
@@ -34,9 +34,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(708, 280);
+            this.ClientSize = new System.Drawing.Size(708, 414);
             this.Name = "Form1";
-            this.Text = "Form1";
             this.ResumeLayout(false);
 
         }
