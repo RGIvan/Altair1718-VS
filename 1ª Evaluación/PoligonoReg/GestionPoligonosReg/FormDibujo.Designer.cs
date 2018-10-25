348aa9c5d3e6cad541780332be8586907a7d46dc
RGIvan <cloudchito@gmail.com>
2018-10-25 19:30:17 +0200

    VS
    VS

--- 1ª Evaluación/PoligonoReg/GestionPoligonosReg/FormDibujo.Designer.cs
+++ 1ª Evaluación/PoligonoReg/GestionPoligonosReg/FormDibujo.Designer.cs
@@ -87,7 +87,6 @@
             this.trackY.TabIndex = 1;
             this.trackY.TickFrequency = 10;
             this.trackY.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
-            this.trackY.Scroll += new System.EventHandler(this.trackY_Scroll);
             // 
             // trackX
             // 
@@ -102,7 +101,6 @@
             this.trackX.TabIndex = 1;
             this.trackX.TickFrequency = 10;
             this.trackX.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
-            this.trackX.Scroll += new System.EventHandler(this.trackX_Scroll);
             // 
             // label2
             // 
@@ -201,6 +199,7 @@
             this.txtGiro.Size = new System.Drawing.Size(48, 29);
             this.txtGiro.TabIndex = 35;
             this.txtGiro.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtGiro.TextChanged += new System.EventHandler(this.txtGiro_TextChanged);
             // 
             // trackGiro
             // 
@@ -212,6 +211,7 @@
             this.trackGiro.Name = "trackGiro";
             this.trackGiro.Size = new System.Drawing.Size(258, 30);
             this.trackGiro.TabIndex = 34;
+            this.trackGiro.ValueChanged += new System.EventHandler(this.trackGiro_ValueChanged);
             // 
             // label5
             // 
